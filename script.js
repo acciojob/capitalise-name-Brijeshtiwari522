b6dcf1cd6d449695347bcab02886d6f1c21f45d6
Brijeshtiwari522 <151025248+Brijeshtiwari522@users.noreply.github.com>
2024-11-26 15:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your JS code here. If required.
+//your JS code here. If require
+document.getElementById('fname').addEventListener('blur', function() {
+            this.value = this.value.toUpperCase();
